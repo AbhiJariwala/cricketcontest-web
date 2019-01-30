3d3df723a1cb22de43dbd9dfc864246467b74134
Suhani Shah <lanetteam.suhani@gmail.com>
2019-01-30 12:05:45 +0530

    Changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,7 +37,7 @@ class Footer extends React.Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Abhi Jariwala
+              Abhi Jariwala footer
             </a>.
           </div>
         </Container>
