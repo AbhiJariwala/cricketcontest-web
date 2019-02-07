c0c6c4b2ca6ec629553ab15060e093bafceb55b3
Admin\lcom40-one <dhiraj.shah.ninja@gmail.com>
2019-02-07 17:57:49 +0530

    Nivedita - 7/2/2019 - Validation in TeamPlayer

--- src/views/TournamentTeam/tournamentTeam.js
+++ src/views/TournamentTeam/tournamentTeam.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-import {message} from 'antd';
+import { message } from 'antd';
 import { Input, ButtonGroup, Table, Button } from 'reactstrap';
 import { PanelHeader } from "components";
 import 'antd/dist/antd.css';
@@ -12,6 +12,7 @@ import * as TournamentTeamAction from '../../action/TournamentTeam';
 import AddTournamentTeam from '../TournamentTeam/AddTournament/addTournamentTeam'
 
 import ShowTeams from './showTeams';
+import path from '../../path';
 
 class TournamentTeam extends Component {
   constructor(props) {
@@ -19,17 +20,17 @@ class TournamentTeam extends Component {
     this.state = {
       addModal: false,
       teamModal: false,
-      visible:false,
+      visible: false,
       sort: false,
       pageno: 0,
       parpageRecord: 5,
       sorting: "",
-      tournament:{},
+      tournament: {},
       sortingValueName: "id",
       sortingValue: "desc",
-      teamid:"",
-      tournamentid:"",
-      updatedBy:""
+      teamid: "",
+      tournamentid: "",
+      updatedBy: ""
     };
     this.toggle = this.toggle.bind(this);
   }
@@ -43,24 +44,24 @@ class TournamentTeam extends Component {
   sortingdata = (Event) => {
     const sortingValueName = Event.target.id;
     if (sortingValueName !== "Action") {
-        let sortingValue = "asc";
-        
-        if (!this.state.sortingValueName) {
-          this.setState({ sortingValueName: sortingValueName })
+      let sortingValue = "asc";
+
+      if (!this.state.sortingValueName) {
+        this.setState({ sortingValueName: sortingValueName })
+      }
+      else if (this.state.sortingValueName === sortingValueName) {
+        if (this.state.sortingValue === "asc") {
+          sortingValue = "desc"
+        } else {
+          sortingValue = "asc"
         }
-        else if (this.state.sortingValueName === sortingValueName) {
-          if (this.state.sortingValue === "asc") {
-            sortingValue = "desc"
-          } else {
-            sortingValue = "asc"
-          }
-          this.setState({ sortingValueName: sortingValueName, sortingValue: sortingValue })
-        }
-        else {
-          this.setState({ sortingValueName: sortingValueName, sortingValue: "asc" })
-        }
-        
-        this.props.action.Tournament.fetchTournamentAction(this.state.pageno, this.state.parpageRecord, sortingValue, sortingValueName);
+        this.setState({ sortingValueName: sortingValueName, sortingValue: sortingValue })
+      }
+      else {
+        this.setState({ sortingValueName: sortingValueName, sortingValue: "asc" })
+      }
+
+      this.props.action.Tournament.fetchTournamentAction(this.state.pageno, this.state.parpageRecord, sortingValue, sortingValueName);
     }
   }
 
@@ -74,14 +75,14 @@ class TournamentTeam extends Component {
     let datachangeprevNext = Event.target.value;
     let pageno = 0
     if (datachangeprevNext === "Next") {
-        this.setState({ pageno: this.state.pageno + 5 })
-        
-        if (this.state.pageno === 0) {
-            this.setState({ pageno: this.state.parpageRecord })
-            pageno = this.state.parpageRecord
-        } else {
-          pageno = this.state.pageno + this.state.parpageRecord
-        }
+      this.setState({ pageno: this.state.pageno + 5 })
+
+      if (this.state.pageno === 0) {
+        this.setState({ pageno: this.state.parpageRecord })
+        pageno = this.state.parpageRecord
+      } else {
+        pageno = this.state.pageno + this.state.parpageRecord
+      }
     }
     else if (datachangeprevNext === "Prev") {
       this.setState({ pageno: this.state.pageno - this.state.parpageRecord })
@@ -90,88 +91,90 @@ class TournamentTeam extends Component {
     this.props.action.Tournament.fetchTournamentAction(pageno, this.state.parpageRecord, this.state.sortingValue, this.state.sortingValueName);
   }
 
-  toggle(){
+  toggle() {
     this.setState({
       addModal: !this.state.addModal,
       Editdataid: null,
     });
   }
-  
-  toggleTeam=()=>{
+
+  toggleTeam = () => {
     this.setState({
       visible: !this.state.visible
     });
   }
 
   ShowTeam = (tournament) => {
-    if (!tournament.Teams||tournament.Teams===[]) {
+    if (!tournament.Teams || tournament.Teams === []) {
       alert("no teams in tournament");
     } else {
-        this.setState(
-          {
-            tournament:tournament,
-            visible: true
-          })
+      this.setState(
+        {
+          tournament: tournament,
+          visible: true
+        })
     }
   }
 
-  handleDelete = (tournamnetId,team) => {
+  handleDelete = (tournamnetId, team) => {
     message.success("successfully deleted");
     this.toggleTeam();
-    let updatedBy=parseInt(this.state.updatedBy,10);
-    team.map(teamId=>{
-      this.props.action.TournamentTeam.DeleteTournamentTeamAction(tournamnetId,teamId,updatedBy);
+    let updatedBy = parseInt(this.state.updatedBy, 10);
+    team.map(teamId => {
+      this.props.action.TournamentTeam.DeleteTournamentTeamAction(tournamnetId, teamId, updatedBy);
       return teamId;
-      })
+    })
   }
 
   render() {
     let notNext = 0;
     let data = "";
     if (this.props.ShowTournamentAll && this.props.ShowTournamentAll.length > 0) {
-        data = this.props.ShowTournamentAll.map((tournament, key) => {
+      data = this.props.ShowTournamentAll.map((tournament, key) => {
         notNext = key + 1
-        return  <tr key={key} style={{ textAlign: "center" }}>
-                  <td>{tournament.tournamentName}</td>
-                  <td>
-                    <Button color="info" onClick={()=>this.ShowTeam(tournament)}>Show Teams</Button>
-                  </td>
-                </tr>
-        })
+        return <tr key={key} style={{ textAlign: "center" }}>
+          <td><img src={path + tournament.tournamentBanner} alt="Banner" style={{ width: "150px", height: "80px" }}></img></td>
+          <td>{tournament.tournamentName}</td>
+          <td>
+            <Button color="info" onClick={() => this.ShowTeam(tournament)}>Show Teams</Button>
+          </td>
+        </tr>
+      })
     }
     return (
       <div>
         <PanelHeader size="sm" />
         <div className="content"  >
-          <AddTournamentTeam isOpen={this.state.addModal} toggle={this.toggle}/>
-            
-                     <ShowTeams tournament={this.state.tournament} 
-                                teamid={this.state.teamid} 
-                                tournamentid={this.state.tournamentid} 
-                                deleteClick={this.handleDelete} 
-                                visible={this.state.visible}
-                                toggleTeam={this.toggleTeam}
-                                />
-            <div style={{ marginTop: "50px" }}>
-              <div style={{ float: "right" }}>
-                Show entries
+          <AddTournamentTeam isOpen={this.state.addModal} toggle={this.toggle} />
+
+          <ShowTeams tournament={this.state.tournament}
+            teamid={this.state.teamid}
+            tournamentid={this.state.tournamentid}
+            deleteClick={this.handleDelete}
+            visible={this.state.visible}
+            toggleTeam={this.toggleTeam}
+          />
+          <div style={{ marginTop: "50px" }}>
+            <div style={{ float: "right" }}>
+              Show entries
                 <Input type="select" name="select" id="exampleSelect" onChange={this.parpage.bind(Event)}>
-                  <option>5</option>
-                  <option>10</option>
-                  <option>25</option>
-                  <option>50</option>
-                  <option>100</option>
-                </Input>
-              </div>
-              <div style={{ float: "left" }}>
-                <Button color="info" onClick={this.toggle} style={{ width: "62px" }}>Add </Button>
+                <option>5</option>
+                <option>10</option>
+                <option>25</option>
+                <option>50</option>
+                <option>100</option>
+              </Input>
             </div>
-        </div>
-          { data ?
+            <div style={{ float: "left" }}>
+              <Button color="info" onClick={this.toggle} style={{ width: "62px" }}>Add </Button>
+            </div>
+          </div>
+          {data ?
             <Table responsive hover>
               <thead className="thead-dark">
                 <tr style={{ textAlign: "center" }} onClick={this.sortingdata.bind(Event)}>
-                  <th id="tournamentName">Tournament</th>
+                  <th>Banner</th>
+                  <th id="tournamentName" style={{ cursor: "pointer" }}>Tournament</th>
                   <th>Team</th>
                 </tr>
               </thead>
@@ -181,13 +184,13 @@ class TournamentTeam extends Component {
             </Table>
             : ""}
           <ButtonGroup>
-            { this.state.pageno !== 0 ?
+            {this.state.pageno !== 0 ?
               <Button color="info" onClick={this.changeRecord.bind(Event)} value="Prev">Prev</Button> : <Button color="info" onClick={this.changeRecord.bind(Event)} value="Prev" disabled> Prev </Button>
             }
             &nbsp;
-            { notNext >= this.state.parpageRecord ?
+            {notNext >= this.state.parpageRecord ?
               <Button color="info" onClick={this.changeRecord.bind(Event)} value="Next">Next</Button> :
-            ""}
+              ""}
           </ButtonGroup>
         </div>
       </div>
@@ -197,15 +200,15 @@ class TournamentTeam extends Component {
 
 const mapStateToProps = (state) => {
   return {
-            ShowTournamentAll: state.Tournament.Tournaments,
-        }
+    ShowTournamentAll: state.Tournament.Tournaments,
+  }
 };
 
 const mapDispatchToProps = dispatch => ({
   action: {
-            Tournament: bindActionCreators(TournamentAction, dispatch),
-            TournamentTeam: bindActionCreators(TournamentTeamAction, dispatch)
-          }
+    Tournament: bindActionCreators(TournamentAction, dispatch),
+    TournamentTeam: bindActionCreators(TournamentTeamAction, dispatch)
+  }
 });
 export default connect(mapStateToProps, mapDispatchToProps)(TournamentTeam)
 
