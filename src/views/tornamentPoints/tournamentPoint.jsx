becfb35514f3ff0599de9e86ff8aa5d63e23c11c
manasn <manasn@lanetteam.com>
2019-02-08 18:09:24 +0530

    Mitul - 8/2/2019 - Tournament Point

--- src/views/tornamentPoints/tournamentPoint.jsx
+++ src/views/tornamentPoints/tournamentPoint.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { Table, Button, Input, ButtonGroup } from 'reactstrap';
+import { Table, ButtonGroup, Container, Button, Input } from 'reactstrap';
 import { PanelHeader } from "components";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Collapse, Button as AntButton, Modal as AntModal } from 'antd';
+import 'antd/dist/antd.css';
 
 import AddTournamentPoint from './AddTounamnetPoint/addTournamentPoint';
 import * as tournamentPointAction from '../../action/tournamentPoint';
@@ -12,15 +14,19 @@ class tournamentPoint extends Component {
     super(props);
     this.state = {
       modal: false,
+      visible: false,
       offset: 0,
       perPageRecord: 5,
       orderByName: "id",
-      orderBy: "desc"
+      orderBy: "desc",
+      pointData: {}
     };
     this.toggle = this.toggle.bind(this);
+    this.togglePoints = this.togglePoints.bind(this);
     this.perPage = this.perPage.bind(this);
     this.changeRecord = this.changeRecord.bind(this);
-    this.getPoints= this.getPoints.bind(this);
+    this.getPoints = this.getPoints.bind(this);
+    this.renderPointModal = this.renderPointModal.bind(this);
   }
 
   componentDidMount() {
@@ -33,8 +39,11 @@ class tournamentPoint extends Component {
     });
   }
 
-  getPoints(){
-
+  togglePoints() {
+    this.setState({
+      visible: !this.state.visible
+    });
+    this.setState({ pointData: {} });
   }
 
   perPage = (event) => {
@@ -62,16 +71,197 @@ class tournamentPoint extends Component {
     this.props.action.TournamentPoint.getTournamentPointScore(offset, this.state.perPageRecord, this.state.orderByName, this.state.orderBy);
   }
 
+  getPoints(tournamentPoint) {
+    this.togglePoints();
+    this.setState({ pointData: tournamentPoint });
+  }
+
+  renderPointModal() {
+    let pointJson = "";
+    if (this.state.pointData.pointJson) {
+      pointJson = this.state.pointData.pointJson;
+    }
+
+    let runsPoints = null, runsArr = [];
+    if (pointJson.Runs) {
+      runsArr.push(Object.values(pointJson.Runs));
+      runsPoints = runsArr[0].map((run, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{run.from + " - " + run.to}</td>
+            <td>{run.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    let sixPoints = null, sixArr = [];
+    if (pointJson.Six) {
+      sixArr.push(Object.values(pointJson.Six));
+      sixPoints = sixArr[0].map((six, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{six.from + " - " + six.to}</td>
+            <td>{six.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    let fourPoints = null, fourArr = [];
+    if (pointJson.Four) {
+      fourArr.push(Object.values(pointJson.Four));
+      fourPoints = fourArr[0].map((four, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{four.from + " - " + four.to}</td>
+            <td>{four.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    let wicketPoints = null, wicketArr = [];
+    if (pointJson.Wicket) {
+      wicketArr.push(Object.values(pointJson.Wicket));
+      wicketPoints = wicketArr[0].map((wicket, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{wicket.from + " - " + wicket.to}</td>
+            <td>{wicket.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    let stumpingPoints = null, stumpingArr = [];
+    if (pointJson.Stumping) {
+      stumpingArr.push(Object.values(pointJson.Stumping));
+      stumpingPoints = stumpingArr[0].map((stumping, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{stumping.from + " - " + stumping.to}</td>
+            <td>{stumping.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    let catchPoints = null, catchArr = [];
+    if (pointJson.Catch) {
+      catchArr.push(Object.values(pointJson.Catch));
+      catchPoints = catchArr[0].map((catches, i) => {
+        return (
+          <tr key={i} style={{ textAlign: "center" }}>
+            <td>{catches.from + " - " + catches.to}</td>
+            <td>{catches.point}</td>
+          </tr>
+        )
+      })
+    }
+
+    return (
+      <Container>
+        <AntModal
+          title={this.state.pointData.Tournament
+            ? this.state.pointData.Tournament.tournamentName
+            : "Tournamnet"}
+          visible={this.state.visible}
+          onCancel={this.togglePoints}
+          footer={<AntButton type="primary" onClick={this.togglePoints}>Ok</AntButton>}>
+          {
+            (this.state.pointData)
+              ? <Collapse key="Runs" accordion>
+                  <Collapse.Panel header="Runs" key="Runs">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{runsPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                  <Collapse.Panel header="Six" key="Six">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{sixPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                  <Collapse.Panel header="Four" key="Four">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{fourPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                  <Collapse.Panel header="Wicket" key="Wicket">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{wicketPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                  <Collapse.Panel header="Stumping" key="Stumping">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{stumpingPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                  <Collapse.Panel header="Catch" key="Catch">
+                    <Table hover>
+                      <thead>
+                        <tr style={{ textAlign: "center" }}>
+                          <th>Range</th>
+                          <th>Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>{catchPoints}</tbody>
+                    </Table>
+                  </Collapse.Panel>
+
+                </Collapse>
+              : "No Data"
+          }
+        </AntModal>
+      </Container>
+    );
+  }
+
   render() {
     let total = 0;
     let tournamentPoints = "";
     if (this.props.TournamentPoint) {
-      tournamentPoints = this.props.TournamentPoint.map((tournamentPoint,i) => {
+      tournamentPoints = this.props.TournamentPoint.map((tournamentPoint, i) => {
         total = i + 1;
         return (
           <tr key={tournamentPoint.id} style={{ textAlign: "center" }}>
             <td>{tournamentPoint.Tournament.tournamentName}</td>
-            <td><Button color="info" onClick={this.getPoints}>Points</Button></td>
+            <td><Button color="info" onClick={() => this.getPoints(tournamentPoint)}>Points</Button></td>
           </tr>
         )
       })
@@ -80,15 +270,15 @@ class tournamentPoint extends Component {
     return (
       <div>
         <PanelHeader size="sm" />
-        <div className="content">
+          <div className="content">
 
           <AddTournamentPoint isOpen={this.state.modal} toggle={this.toggle}></AddTournamentPoint>
-
+          {this.renderPointModal()}
           <div style={{ marginTop: "50px" }}>
             <div style={{ float: "left" }}>
               <Button color="info" onClick={this.toggle}>Add Points</Button>
             </div>
-            <div style={{ float: "right" }}>
+            <div style={{ float: "right", marginRight: "45px" }}>
               Show entries
               <Input type="select" name="entries" id="exampleSelect" onChange={this.perPage}>
                 <option>5</option>
